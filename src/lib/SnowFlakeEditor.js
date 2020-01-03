509e4341651500e9594a5534aa0e3c5b7a0eb0a7
yoksel <yokselzok@gmail.com>
2020-01-03 11:41:11 +0300

    Fix formatting

--- src/lib/SnowFlakeEditor.js
+++ src/lib/SnowFlakeEditor.js
@@ -1,279 +1,279 @@
 const template = document.createElement('template');
-  template.innerHTML = `
-    <style>
-      :host {
-        display: flex;
-        flex-direction: column;
-        width: 520px;
+template.innerHTML = `
+  <style>
+    :host {
+      display: flex;
+      flex-direction: column;
+      width: 520px;
 
-        --transition: .15s;
-      }
-      :host:focus {
-        outline: none;
-      }
+      --transition: .15s;
+    }
+    :host:focus {
+      outline: none;
+    }
 
-      :host svg {
-        width: 520px;
-        height: 600px;
-      }
+    :host svg {
+      width: 520px;
+      height: 600px;
+    }
 
-      :host path {
-        transition: stroke var(--transition);
-      }
+    :host path {
+      transition: stroke var(--transition);
+    }
 
-      :host .hovered {
-        stroke: aqua;
-        cursor: pointer;
-      }
+    :host .hovered {
+      stroke: aqua;
+      cursor: pointer;
+    }
 
-      :host .highlight {
-        stroke: orangered;
-        cursor: pointer;
-      }
+    :host .highlight {
+      stroke: orangered;
+      cursor: pointer;
+    }
 
-      .controls {
-        margin-top: 2rem;
-      }
+    .controls {
+      margin-top: 2rem;
+    }
 
-      .control {
-        padding: .25rem 1rem;
-        background: #FFF;
-        border: 0;
-        border-radius: .25rem;
-        font: inherit;
-        cursor: pointer;
-        transition: all var(--transition);
-      }
-      .control:hover {
-        background: none;
-        box-shadow:  0 0 0 3px;
-        color: #FFF;
-      }
-    </style>
+    .control {
+      padding: .25rem 1rem;
+      background: #FFF;
+      border: 0;
+      border-radius: .25rem;
+      font: inherit;
+      cursor: pointer;
+      transition: all var(--transition);
+    }
+    .control:hover {
+      background: none;
+      box-shadow:  0 0 0 3px;
+      color: #FFF;
+    }
+  </style>
 
-    <svg viewBox="0 0 260 300">
-      <defs>
-        <path id="shape" d="M130.1,300.5, 130.1,0, 0,75z"></path>
+  <svg viewBox="0 0 260 300">
+    <defs>
+      <path id="shape" d="M130.1,300.5, 130.1,0, 0,75z"></path>
 
-        <clipPath id="slice-clip-path">
-          <use xlink:href="#shape"/>
-        </clipPath>
+      <clipPath id="slice-clip-path">
+        <use xlink:href="#shape"/>
+      </clipPath>
 
-        <symbol id="slice-123">
-          <use xlink:href="#shape"
-            stroke="currentColor"
-            stroke-dasharray="5 2.5"
-            stroke-width=".5"
-            stroke-opacity=".5"
-            fill="none"/>
-          <g clip-path="url(#slice-clip-path)">
-            <rect fill="none" x="0" y="0" width="100%" height="100%"></rect>
-            <g id="target-group-123" stroke="currentColor" stroke-linecap="round" stroke-width="5"></g>
-          </g>
-        </symbol>
-      </defs>
-
-      <rect id="measure-rect" fill="transparent" x="0" y="0" width="100" height="100"></rect>
-
-      <g id="slice">
+      <symbol id="slice-123">
         <use xlink:href="#shape"
           stroke="currentColor"
           stroke-dasharray="5 2.5"
           stroke-width=".5"
           stroke-opacity=".5"
           fill="none"/>
-        <g clip-path="url(#slice-clip-path)" fill="none">
-          <rect x="0" y="0" width="100%" height="100%"></rect>
-          <g id="target-group" stroke="currentColor" stroke-linecap="round" stroke-width="5">
-          </g>
+        <g clip-path="url(#slice-clip-path)">
+          <rect fill="none" x="0" y="0" width="100%" height="100%"></rect>
+          <g id="target-group-123" stroke="currentColor" stroke-linecap="round" stroke-width="5"></g>
+        </g>
+      </symbol>
+    </defs>
+
+    <rect id="measure-rect" fill="transparent" x="0" y="0" width="100" height="100"></rect>
+
+    <g id="slice">
+      <use xlink:href="#shape"
+        stroke="currentColor"
+        stroke-dasharray="5 2.5"
+        stroke-width=".5"
+        stroke-opacity=".5"
+        fill="none"/>
+      <g clip-path="url(#slice-clip-path)" fill="none">
+        <rect x="0" y="0" width="100%" height="100%"></rect>
+        <g id="target-group" stroke="currentColor" stroke-linecap="round" stroke-width="5">
         </g>
       </g>
+    </g>
 
-      <use
-        xlink:href="#slice"
-        transform="scale(-1,1)"
-        style="transform-origin: 130px 0; opacity:.25"
-        />
-    </svg>
+    <use
+      xlink:href="#slice"
+      transform="scale(-1,1)"
+      style="transform-origin: 130px 0; opacity:.25"
+      />
+  </svg>
 
-    <div class="controls">
-      <button type="button" class="control control--clear">Clear</button>
-    </div>
-  `;
+  <div class="controls">
+    <button type="button" class="control control--clear">Clear</button>
+  </div>
+`;
 
 export default class SnowFlakeEditor extends HTMLElement {
-    constructor() {
-      super();
-      this.attachShadow({mode: 'open'});
-      this.elem = this.shadowRoot;
-      this.elem.appendChild(template.content.cloneNode(true));
-      this.targetGroup = this.elem.querySelector('#target-group');
-      this.controlClear = this.elem.querySelector('.control--clear');
-      this.scale = this.getScale();
-      this.clickedPath = null;
-      this.pathsCounter = 0;
-      this.selected = {};
+  constructor() {
+    super();
+    this.attachShadow({mode: 'open'});
+    this.elem = this.shadowRoot;
+    this.elem.appendChild(template.content.cloneNode(true));
+    this.targetGroup = this.elem.querySelector('#target-group');
+    this.controlClear = this.elem.querySelector('.control--clear');
+    this.scale = this.getScale();
+    this.clickedPath = null;
+    this.pathsCounter = 0;
+    this.selected = {};
 
-      this.targetGroupMouseDown = this.targetGroupMouseDown.bind(this);
-      this.mouseOverPath = this.mouseOverPath.bind(this);
-      this.mouseOutPath = this.mouseOutPath.bind(this);
-      this.clear = this.clear.bind(this);
-      this.unselect = this.unselect.bind(this);
-    }
+    this.targetGroupMouseDown = this.targetGroupMouseDown.bind(this);
+    this.mouseOverPath = this.mouseOverPath.bind(this);
+    this.mouseOutPath = this.mouseOutPath.bind(this);
+    this.clear = this.clear.bind(this);
+    this.unselect = this.unselect.bind(this);
+  }
 
-    connectedCallback() {
-      this.addEventListener('mousedown', this.mouseDown);
-      this.addEventListener('mouseup', this.mouseUp);
-      // Catch  event from parent document
-      this.addEventListener('remove-path', this.removePaths);
-      // Catch events for path inside paths group
-      this.targetGroup.addEventListener('mousedown', this.targetGroupMouseDown);
+  connectedCallback() {
+    this.addEventListener('mousedown', this.mouseDown);
+    this.addEventListener('mouseup', this.mouseUp);
+    // Catch event from parent document
+    this.addEventListener('remove-path', this.removePaths);
+    // Catch events for path inside paths group
+    this.targetGroup.addEventListener('mousedown', this.targetGroupMouseDown);
 
-      this.controlClear.addEventListener('click', this.clear);
-    }
+    this.controlClear.addEventListener('click', this.clear);
+  }
 
-    disconnectedCallback() {
-      // add later
-      // this.removeEventListener('mousedown', this.mouseDown);
-      // this.removeEventListener('mouseup', this.mouseUp);
-    }
+  disconnectedCallback() {
+    // add later
+    // this.removeEventListener('mousedown', this.mouseDown);
+    // this.removeEventListener('mouseup', this.mouseUp);
+  }
 
-    mouseDown(event) {
-      // Start draw line, create path
-      this.path = {
-        elem: document.createElementNS('http://www.w3.org/2000/svg', 'path'),
-        start: {
-          x: this.scaleCoord(event.offsetX),
-          y: this.scaleCoord(event.offsetY)
-        }
-      };
-
-      this.targetGroup.append(this.path.elem);
-
-      this.addEventListener('mousemove', this.mouseMove);
-    }
-
-    mouseMove(event) {
-      // Move path end to pointer coordinates
-      this.path.elem.setAttribute(
-        'd',
-        `M${this.path.start.x},${this.path.start.y} ${this.scaleCoord(event.offsetX)},${this.scaleCoord(event.offsetY)}`);
-
-      this.clickedPath = null;
-    }
-
-    mouseUp(event) {
-      // Catch click to highlight path
-      if(this.clickedPath) {
-        this.mouseClickPath();
-
-        // Remove latest path
-        this.path.elem.remove();
-        this.removeEventListener('mousemove', this.mouseMove);
-        return;
+  mouseDown(event) {
+    // Start draw line, create path
+    this.path = {
+      elem: document.createElementNS('http://www.w3.org/2000/svg', 'path'),
+      start: {
+        x: this.scaleCoord(event.offsetX),
+        y: this.scaleCoord(event.offsetY)
       }
+    };
 
-      this.unselect();
+    this.targetGroup.append(this.path.elem);
 
-      // Or fix new path
-      this.path.elem.id = `path-${this.pathsCounter}`;
-      this.pathsCounter++;
+    this.addEventListener('mousemove', this.mouseMove);
+  }
 
-      this.path.elem.setAttribute(
-        'd',
-        `M${this.path.start.x},${this.path.start.y} ${this.scaleCoord(event.offsetX)},${this.scaleCoord(event.offsetY)}`);
+  mouseMove(event) {
+    // Move path end to pointer coordinates
+    this.path.elem.setAttribute(
+      'd',
+      `M${this.path.start.x},${this.path.start.y} ${this.scaleCoord(event.offsetX)},${this.scaleCoord(event.offsetY)}`);
 
+    this.clickedPath = null;
+  }
+
+  mouseUp(event) {
+    // Catch click to highlight path
+    if(this.clickedPath) {
+      this.mouseClickPath();
+
+      // Remove latest path
+      this.path.elem.remove();
       this.removeEventListener('mousemove', this.mouseMove);
-      this.dispatchChange();
-
-      // Add events to new path
-      this.path.elem.addEventListener('mouseover', this.mouseOverPath);
-      this.path.elem.addEventListener('mouseout', this.mouseOutPath);
+      return;
     }
 
-    mouseOverPath(event) {
-      event.target.classList.add('hovered');
-    }
+    this.unselect();
 
-    mouseOutPath(event) {
-      event.target.classList.remove('hovered');
-    }
+    // Or fix new path
+    this.path.elem.id = `path-${this.pathsCounter}`;
+    this.pathsCounter++;
 
-    mouseClickPath() {
-      const pathId = this.clickedPath.id;
-        this.clickedPath.classList.toggle('highlight');
+    this.path.elem.setAttribute(
+      'd',
+      `M${this.path.start.x},${this.path.start.y} ${this.scaleCoord(event.offsetX)},${this.scaleCoord(event.offsetY)}`);
 
-        if(this.clickedPath.classList.contains('highlight')) {
-          this.selected[pathId] = this.clickedPath;
-        }
-        else {
-          delete this.selected[pathId];
+    this.removeEventListener('mousemove', this.mouseMove);
+    this.dispatchChange();
+
+    // Add events to new path
+    this.path.elem.addEventListener('mouseover', this.mouseOverPath);
+    this.path.elem.addEventListener('mouseout', this.mouseOutPath);
+  }
+
+  mouseOverPath(event) {
+    event.target.classList.add('hovered');
+  }
+
+  mouseOutPath(event) {
+    event.target.classList.remove('hovered');
+  }
+
+  mouseClickPath() {
+    const pathId = this.clickedPath.id;
+      this.clickedPath.classList.toggle('highlight');
+
+      if(this.clickedPath.classList.contains('highlight')) {
+        this.selected[pathId] = this.clickedPath;
       }
-
-      this.clickedPath = null;
+      else {
+        delete this.selected[pathId];
     }
 
-    dispatchChange() {
-      this.dispatchEvent(new CustomEvent('change', {
-        detail: {
-          checked: this.checked,
-          groupContent: this.getSinglePathStr()
-        },
-        bubbles: true,
-      }));
+    this.clickedPath = null;
+  }
+
+  dispatchChange() {
+    this.dispatchEvent(new CustomEvent('change', {
+      detail: {
+        checked: this.checked,
+        groupContent: this.getSinglePathStr()
+      },
+      bubbles: true,
+    }));
+  }
+
+  getSinglePathStr() {
+    this.targetGroup.children.map = [].map;
+    let singlePathCoords = '';
+
+    for(let item of this.targetGroup.children) {
+      singlePathCoords += ` ${item.getAttribute('d')}`;
     }
 
-    getSinglePathStr() {
-      this.targetGroup.children.map = [].map;
-      let singlePathCoords = '';
+    return `<path d="${singlePathCoords}"/>`;
+  }
 
-      for(let item of this.targetGroup.children) {
-        singlePathCoords += ` ${item.getAttribute('d')}`;
+  targetGroupMouseDown(event) {
+    this.clickedPath = event.target;
+  }
+
+  unselect() {
+    if(Object.values(this.selected).length > 0) {
+      for(let key in this.selected) {
+        this.selected[key].classList.remove('highlight');
       }
-
-      return `<path d="${singlePathCoords}"/>`;
-    }
-
-    targetGroupMouseDown(event) {
-      this.clickedPath = event.target;
-    }
-
-    unselect() {
-      if(Object.values(this.selected).length > 0) {
-        for(let key in this.selected) {
-          this.selected[key].classList.remove('highlight');
-        }
-      }
-    }
-
-    removePaths() {
-      if(Object.values(this.selected).length > 0) {
-        for(let key in this.selected) {
-          console.log(key)
-          this.selected[key].remove();
-          delete this.selected[key];
-        }
-
-        this.dispatchChange();
-      }
-    }
-
-    clear() {
-      this.targetGroup.innerHTML = '';
-      this.selected = {};
-      this.dispatchChange();
-    }
-
-    // Return scale value if svg was resized
-    getScale() {
-      const measurePath = this.elem.getElementById('measure-rect');
-      const width = measurePath.getAttribute('width');
-      const rect = measurePath.getBoundingClientRect();
-
-      return width / rect.width;
-    }
-
-    scaleCoord(coord) {
-      return coord * this.scale;
     }
   }
+
+  removePaths() {
+    if(Object.values(this.selected).length > 0) {
+      for(let key in this.selected) {
+        console.log(key)
+        this.selected[key].remove();
+        delete this.selected[key];
+      }
+
+      this.dispatchChange();
+    }
+  }
+
+  clear() {
+    this.targetGroup.innerHTML = '';
+    this.selected = {};
+    this.dispatchChange();
+  }
+
+  // Return scale value if svg was resized
+  getScale() {
+    const measurePath = this.elem.getElementById('measure-rect');
+    const width = measurePath.getAttribute('width');
+    const rect = measurePath.getBoundingClientRect();
+
+    return width / rect.width;
+  }
+
+  scaleCoord(coord) {
+    return coord * this.scale;
+  }
+}
--- src/lib/SnowFlakeView.js
+++ src/lib/SnowFlakeView.js
@@ -1,94 +1,94 @@
 const template = document.createElement('template');
-  template.innerHTML = `
-    <style>
-      :host {
-        position: relative;
-        display: block;
-        min-width: 300px;
-        min-height: 300px;
-      }
+template.innerHTML = `
+  <style>
+    :host {
+      position: relative;
+      display: block;
+      min-width: 300px;
+      min-height: 300px;
+    }
 
-      :host svg {
-        width: 100%;
-        height: 100%;
-      }
+    :host svg {
+      width: 100%;
+      height: 100%;
+    }
 
-      :host .contour {
-        left: 0;
-        top: 0;
-        position: absolute;
-        stroke-opacity: .5;
-        transition: stroke-opacity .25s;
-      }
-      :host .contour--hidden {
-        stroke-opacity: 0;
-      }
-    </style>
+    :host .contour {
+      left: 0;
+      top: 0;
+      position: absolute;
+      stroke-opacity: .5;
+      transition: stroke-opacity .25s;
+    }
+    :host .contour--hidden {
+      stroke-opacity: 0;
+    }
+  </style>
 
-    <svg class="contour" viewBox="0 0 600 600" hidden>
-      <circle r="300" cx="300" cy="300"
-        stroke="currentColor"
-        stroke-dasharray="10 5"
-        stroke-width="1"
-        fill="none"/>
-    </svg>
+  <svg class="contour" viewBox="0 0 600 600" hidden>
+    <circle r="300" cx="300" cy="300"
+      stroke="currentColor"
+      stroke-dasharray="10 5"
+      stroke-width="1"
+      fill="none"/>
+  </svg>
 
-    <svg class="snowflake" viewBox="0 0 600 600">
-      <defs>
-        <path id="shape" d="M130.1,300.5, 130.1,0, 0,75z"></path>
-      </defs>
+  <svg class="snowflake" viewBox="0 0 600 600">
+    <defs>
+      <path id="shape" d="M130.1,300.5, 130.1,0, 0,75z"></path>
+    </defs>
 
-      <clipPath id="slice-clip-path">
-        <use xlink:href="#shape"/>
-      </clipPath>
+    <clipPath id="slice-clip-path">
+      <use xlink:href="#shape"/>
+    </clipPath>
 
-      <symbol id="slice">
-        <g clip-path="url(#slice-clip-path)">
-          <g id="target-group" stroke="currentColor" stroke-linecap="round" stroke-width="5"></g>
-        </g>
-      </symbol>
-
-      <symbol id="pair-slices">
-        <g transform="translate(170)">
-          <use xlink:href="#slice"/>
-          <use xlink:href="#slice" transform="scale(-1,1)" style="transform-origin: 130px 0"/>
-        </g>
-      </symbol>
-
-      <g>
-        <use xlink:href="#pair-slices"
-             transform="rotate(30, 300, 300)"/>
-        <use xlink:href="#pair-slices"
-             transform="rotate(90, 300, 300)"/>
-        <use xlink:href="#pair-slices"
-             transform="rotate(150, 300, 300)"/>
-        <use xlink:href="#pair-slices"
-             transform="rotate(210, 300, 300)"/>
-        <use xlink:href="#pair-slices"
-             transform="rotate(270, 300, 300)"/>
-        <use xlink:href="#pair-slices"
-             transform="rotate(330, 300, 300)"/>
+    <symbol id="slice">
+      <g clip-path="url(#slice-clip-path)">
+        <g id="target-group" stroke="currentColor" stroke-linecap="round" stroke-width="5"></g>
       </g>
-    </svg>
-  `;
+    </symbol>
+
+    <symbol id="pair-slices">
+      <g transform="translate(170)">
+        <use xlink:href="#slice"/>
+        <use xlink:href="#slice" transform="scale(-1,1)" style="transform-origin: 130px 0"/>
+      </g>
+    </symbol>
+
+    <g>
+      <use xlink:href="#pair-slices"
+           transform="rotate(30, 300, 300)"/>
+      <use xlink:href="#pair-slices"
+           transform="rotate(90, 300, 300)"/>
+      <use xlink:href="#pair-slices"
+           transform="rotate(150, 300, 300)"/>
+      <use xlink:href="#pair-slices"
+           transform="rotate(210, 300, 300)"/>
+      <use xlink:href="#pair-slices"
+           transform="rotate(270, 300, 300)"/>
+      <use xlink:href="#pair-slices"
+           transform="rotate(330, 300, 300)"/>
+    </g>
+  </svg>
+`;
 
 
 
 export default class SnowFlakeView extends HTMLElement {
-  constructor() {
-    super();
-    this.attachShadow({mode: 'open'});
-    this.elem = this.shadowRoot;
-    this.elem.appendChild(template.content.cloneNode(true));
-    this.targetGroup = this.elem.querySelector('#target-group');
-  }
+constructor() {
+  super();
+  this.attachShadow({mode: 'open'});
+  this.elem = this.shadowRoot;
+  this.elem.appendChild(template.content.cloneNode(true));
+  this.targetGroup = this.elem.querySelector('#target-group');
+}
 
-  connectedCallback() {
-    this.addEventListener('change-view', this.changeView);
-  }
+connectedCallback() {
+  this.addEventListener('change-view', this.changeView);
+}
 
-  changeView() {
-    this.targetGroup.innerHTML = event.detail.groupContent;
+changeView() {
+  this.targetGroup.innerHTML = event.detail.groupContent;
   }
 
   disconnectedCallback() {
